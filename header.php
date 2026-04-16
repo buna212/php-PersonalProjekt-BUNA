89894305fe1836bcdb925b490c0876b98f333ded
buna212 <bunaberisha27@outlook.com>
2026-04-16 20:42:08 +0200

    gskh

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIGN UP !!!</title>
+    <title>Urban Doll NYC</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
@@ -15,7 +15,7 @@
   <div class="orb orb-3"></div>
 </div>
 
-<!-- Navbar -->
+
 <nav class="navbar navbar-expand-lg fixed-top navbar-glass py-2" id="mainNav">
   <div class="container">
     <a class="navbar-brand" href="#">Lumière</a>
